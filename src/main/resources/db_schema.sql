3b990aedda1f8841d16ccb44df35d74fe6119ae1
Dusan Mijailovic <dusanmijailovic@outlook.com>
2017-06-06 01:37:28 +0200

    Added SQL database schema structure dump file

---
+++ src/main/resources/db_schema.sql
@@ -0,0 +1,145 @@
+CREATE DATABASE  IF NOT EXISTS `demo_schema` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `demo_schema`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: demo_schema
+-- ------------------------------------------------------
+-- Server version	5.7.17-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `player`
+--
+
+DROP TABLE IF EXISTS `player`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `player` (
+  `player_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `team_id` bigint(20) NOT NULL,
+  `nickname` varchar(45) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `picture` blob,
+  `birthyear` int(11) DEFAULT NULL,
+  `contact` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`player_id`),
+  KEY `username_idx` (`nickname`),
+  KEY `fk_player_team_id_idx` (`team_id`),
+  CONSTRAINT `fk_player_team_id` FOREIGN KEY (`team_id`) REFERENCES `team` (`team_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `public_game`
+--
+
+DROP TABLE IF EXISTS `public_game`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `public_game` (
+  `public_game_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `message` text NOT NULL,
+  `contact` varchar(60) NOT NULL,
+  `state` varchar(45) NOT NULL,
+  `city` varchar(45) NOT NULL,
+  `address` varchar(45) NOT NULL,
+  `date` varchar(45) NOT NULL,
+  `time` varchar(45) NOT NULL,
+  PRIMARY KEY (`public_game_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `public_game_team`
+--
+
+DROP TABLE IF EXISTS `public_game_team`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `public_game_team` (
+  `public_game_id` int(10) unsigned NOT NULL,
+  `team_id` bigint(20) NOT NULL,
+  KEY `fk_public_game_team_public_game_id_idx` (`public_game_id`),
+  KEY `fk_public_game_team_team_id_idx` (`team_id`),
+  CONSTRAINT `fk_public_game_team_public_game_id` FOREIGN KEY (`public_game_id`) REFERENCES `public_game` (`public_game_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_public_game_team_team_id` FOREIGN KEY (`team_id`) REFERENCES `team` (`team_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `request`
+--
+
+DROP TABLE IF EXISTS `request`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `request` (
+  `request_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `sent_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `message` text NOT NULL,
+  `sender_team_id` bigint(20) NOT NULL,
+  `receiver_team_id` bigint(20) NOT NULL,
+  `status` bit(1) NOT NULL DEFAULT b'0',
+  `contact` varchar(60) NOT NULL,
+  `state` varchar(45) NOT NULL,
+  `city` varchar(45) NOT NULL,
+  `address` varchar(45) NOT NULL,
+  `date` varchar(45) NOT NULL,
+  `time` varchar(45) NOT NULL,
+  PRIMARY KEY (`request_id`),
+  KEY `fk_request_receiver_team_id_idx` (`receiver_team_id`),
+  KEY `fk_request_sender_team_id_idx` (`sender_team_id`),
+  CONSTRAINT `fk_request_receiver_team_id` FOREIGN KEY (`receiver_team_id`) REFERENCES `team` (`team_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_request_sender_team_id` FOREIGN KEY (`sender_team_id`) REFERENCES `team` (`team_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `team`
+--
+
+DROP TABLE IF EXISTS `team`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `team` (
+  `team_id` bigint(20) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `state` varchar(60) DEFAULT NULL,
+  `appearance_plus` int(11) NOT NULL DEFAULT '0',
+  `appearance_minus` int(11) NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `open` bit(1) NOT NULL DEFAULT b'1',
+  `active` bit(1) NOT NULL DEFAULT b'1',
+  `email` varchar(88) DEFAULT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  `picture` longblob,
+  `last_edited_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `access_token` varchar(255) NOT NULL,
+  PRIMARY KEY (`team_id`),
+  UNIQUE KEY `access_token_UNIQUE` (`access_token`),
+  UNIQUE KEY `uq_name` (`name`),
+  UNIQUE KEY `uq_email` (`email`),
+  KEY `name_idx` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-06-06  1:35:35
